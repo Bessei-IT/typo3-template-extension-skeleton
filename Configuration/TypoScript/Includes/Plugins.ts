754f07c4d333f95e038849c756af2ebc13f110de
Christoph Bessei <chris@bessei-it.eu>
2017-11-24 23:00:59 +0100

    Add basic config for EXT:form

--- Configuration/TypoScript/Includes/Plugins.ts
+++ Configuration/TypoScript/Includes/Plugins.ts
@@ -1,36 +1,46 @@
-plugin.tx_news {
-    view {
-        templateRootPaths >
-        templateRootPaths {
-            0 = EXT:news/Resources/Private/Templates/
-            5 = EXT:news/Resources/Private/Templates/Styles/Twb/Templates
-            15 = EXT:template/Resources/Private/Fluid/News/Templates/
+plugin {
+    tx_news {
+        view {
+            templateRootPaths >
+            templateRootPaths {
+                0 = EXT:news/Resources/Private/Templates/
+                5 = EXT:news/Resources/Private/Templates/Styles/Twb/Templates
+                15 = EXT:template/Resources/Private/Fluid/News/Templates/
+            }
+
+            partialRootPaths >
+            partialRootPaths {
+                0 = EXT:news/Resources/Private/Partials/
+                5 = EXT:news/Resources/Private/Templates/Styles/Twb/Partials/
+                15 = EXT:template/Resources/Private/Fluid/News/Partials/
+            }
+
+            layoutRootPaths >
+            layoutRootPaths {
+                0 = EXT:news/Resources/Private/Layouts/
+                5 = EXT:news/Resources/Private/Templates/Styles/Twb/Layouts/
+                15 = EXT:template/Resources/Private/Fluid/News/Layouts/
+            }
         }
 
-        partialRootPaths >
-        partialRootPaths {
-            0 = EXT:news/Resources/Private/Partials/
-            5 = EXT:news/Resources/Private/Templates/Styles/Twb/Partials/
-            15 = EXT:template/Resources/Private/Fluid/News/Partials/
-        }
+        settings {
+            displayDummyIfNoMedia = 0
 
-        layoutRootPaths >
-        layoutRootPaths {
-            0 = EXT:news/Resources/Private/Layouts/
-            5 = EXT:news/Resources/Private/Templates/Styles/Twb/Layouts/
-            15 = EXT:template/Resources/Private/Fluid/News/Layouts/
+            list {
+                media.image.width = 400
+            }
+
+            detail {
+                showSocialShareButtons = 0
+            }
         }
     }
 
-    settings {
-        displayDummyIfNoMedia = 0
-
-        list {
-            media.image.width = 400
-        }
-
-        detail {
-            showSocialShareButtons = 0
+    tx_form {
+        settings {
+            yamlConfigurations {
+                1511560813 = EXT:template/Configuration/Form/Frontend.yaml
+            }
         }
     }
 }

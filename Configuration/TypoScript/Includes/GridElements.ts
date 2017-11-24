cef25811cce17109629f20c2825ad8b92baf67ee
Christoph Bessei <chris@bessei-it.eu>
2017-11-24 23:03:34 +0100

    Improve GridElement config for TYPO3 8.7 compatibility

--- Configuration/TypoScript/Includes/GridElements.ts
+++ Configuration/TypoScript/Includes/GridElements.ts
@@ -1,9 +1,31 @@
-#tt_content.gridelements_view.20.10.setup {
-#    GRID_ALIAS < lib.gridelements.defaultGridSetup
-#    GRID_ALIAS {
-#        cObject = FLUIDTEMPLATE
-#        cObject {
-#            file = EXT:template/Resources/Private/Fluid/GridElements/Templates/TEMPLATE.html
+#tt_content.gridelements_pi1 =< lib.contentElement
+#tt_content.gridelements_pi1 {
+#    templateName = Generic
+#    variables {
+#        content =< tt_content.gridelements_view
+#    }
+#}
+#
+#tt_content {
+#    gridelements_pi1 = COA
+#    gridelements_pi1 {
+#        20 {
+#            10 {
+#                setup {
+#                    GRID_ALIAS < lib.gridelements.defaultGridSetup
+#                    GRID_ALIAS {
+#                        cObject = FLUIDTEMPLATE
+#                        cObject {
+#                            layoutRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Layouts/
+#                            partialRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Partials/
+#                            templateRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Templates/
+#                            file = EXT:template/Resources/Private/Fluid/ContentElements/Templates/GridElements/1_1_1_1.html
+#                        }
+#                    }
+#                }
+#            }
 #        }
 #    }
 #}
+#
+#tt_content.gridelements_view < tt_content.gridelements_pi1

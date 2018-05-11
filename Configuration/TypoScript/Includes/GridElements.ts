dd37c26e2d81d226654f3ab771560a75d5c23b3c
Christoph Bessei <chris@bessei-it.eu>
2018-05-11 11:34:08 +0200

    Improve default grid elements config

--- Configuration/TypoScript/Includes/GridElements.ts
+++ Configuration/TypoScript/Includes/GridElements.ts
@@ -1,31 +1,39 @@
-#tt_content.gridelements_pi1 =< lib.contentElement
-#tt_content.gridelements_pi1 {
-#    templateName = Generic
-#    variables {
-#        content =< tt_content.gridelements_view
-#    }
-#}
-#
-#tt_content {
-#    gridelements_pi1 = COA
-#    gridelements_pi1 {
-#        20 {
-#            10 {
-#                setup {
-#                    GRID_ALIAS < lib.gridelements.defaultGridSetup
-#                    GRID_ALIAS {
-#                        cObject = FLUIDTEMPLATE
-#                        cObject {
-#                            layoutRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Layouts/
-#                            partialRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Partials/
-#                            templateRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Templates/
-#                            file = EXT:template/Resources/Private/Fluid/ContentElements/Templates/GridElements/1_1_1_1.html
-#                        }
-#                    }
-#                }
-#            }
-#        }
-#    }
-#}
-#
-#tt_content.gridelements_view < tt_content.gridelements_pi1
+tt_content.gridelements_pi1 =< lib.contentElement
+tt_content.gridelements_pi1 {
+    templateName = Generic
+    variables {
+        content =< tt_content.gridelements_view
+    }
+}
+
+temp.gridelements.defaultCObject = FLUIDTEMPLATE
+temp.gridelements.defaultCObject {
+    layoutRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Layouts/
+    partialRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Partials/
+    templateRootPaths.1511514167 = EXT:template/Resources/Private/Fluid/ContentElements/Templates/
+}
+
+temp.gridelements.equalColumn < temp.gridelements.defaultCObject
+temp.gridelements.equalColumn.file = EXT:template/Resources/Private/Fluid/ContentElements/Templates/GridElements/EqualColumn.html
+
+tt_content {
+    gridelements_pi1 = COA
+    gridelements_pi1 {
+        20 {
+            10 {
+                setup {
+                    twoEqualColumn < lib.gridelements.defaultGridSetup
+                    twoEqualColumn.cObject < temp.gridelements.equalColumn
+
+                    threeEqualColumn < lib.gridelements.defaultGridSetup
+                    threeEqualColumn.cObject < temp.gridelements.equalColumn
+
+                    fourEqualColumn < lib.gridelements.defaultGridSetup
+                    fourEqualColumn.cObject < temp.gridelements.equalColumn
+                }
+            }
+        }
+    }
+}
+
+tt_content.gridelements_view < tt_content.gridelements_pi1

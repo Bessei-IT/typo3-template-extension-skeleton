b0fc291d7412b7dcb10d0fd90d6b618cf599ffdc
Christoph Bessei <chris@bessei-it.eu>
2018-05-11 11:35:03 +0200

    Use .typoscript file extension

--- Configuration/TypoScript/setup.ts
+++
@@ -1,9 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/Config.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/Translations.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/FluidContent.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/GridElements.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/Plugins.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/MetaTags.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/Navigation.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/AnalyticsAndTracking.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Includes/Page.ts">
